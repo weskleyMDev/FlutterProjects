12e3d419ab8c979f877a448f54e0d631134e068c
weskleyMDev <weskleymdev@gmail.com>
2022-09-21 12:21:25 -0300

    perguntas_app main atualizado

--- perguntas_app/lib/main.dart
+++ perguntas_app/lib/main.dart
@@ -7,15 +7,38 @@ void main() {
 class PerguntasApp extends StatelessWidget {
   const PerguntasApp({super.key});
 
+  void responder() {
+    print('Pergunta Respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é seu time favorito?',
+      'Qual é sua cor favorita?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas App'),
         ),
-        body: const Center(
-          child: Text('Ola Mundo'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
         ),
       ),
     );
