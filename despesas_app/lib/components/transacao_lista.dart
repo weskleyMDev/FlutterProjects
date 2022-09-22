6f67b38b1a677e035923adeb43e4306692624602
weskleyMDev <weskleymdev@gmail.com>
2022-09-22 18:33:22 -0300

    Componente lista criado

---
+++ despesas_app/lib/components/transacao_lista.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../model/transacao.dart';
+
+class TransacaoLista extends StatelessWidget {
+  final List<Transacao> transacoes;
+  const TransacaoLista(this.transacoes, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transacoes.map((tr) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple.shade700,
+                    width: 2,
+                  ),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.valor.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple.shade500,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tr.titulo,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(tr.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
--- despesas_app/lib/main.dart
+++ despesas_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:despesas_app/components/transacao_lista.dart';
 import 'package:despesas_app/model/transacao.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +8,15 @@ void main() {
 }
 
 class DespesasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
   final _transacoes = [
     Transacao(
       id: 't1',
@@ -21,8 +32,6 @@ class DespesasApp extends StatelessWidget {
     )
   ];
 
-  DespesasApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,65 +40,49 @@ class DespesasApp extends StatelessWidget {
           title: const Text('Despesas App Flutter'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // ignore: avoid_unnecessary_containers
             Container(
-              child: const Card(
+              child: Card(
                 color: Colors.blue,
                 elevation: 5,
                 child: Text('Gráfico'),
               ),
             ),
-            Column(
-              children: _transacoes.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple.shade700,
-                            width: 2,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.valor.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple.shade500,
-                          ),
-                        ),
+            TransacaoLista(_transacoes),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título',
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.titulo,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            tr.date.toString(),
-                            style: const TextStyle(
-                              color: Colors.green,
-                            ),
-                          ),
-                        ],
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)',
                       ),
-                    ],
-                  ),
-                );
-              }).toList(),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.purple,
+                          ),
+                          child: Text('Nova transação'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
