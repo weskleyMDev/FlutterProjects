9ba41d13113242e1a0acb8b00c45362512f7e1af
Weskley_Dev <weskleymdev@gmail.com>
2022-10-01 11:46:52 -0300

    novo projeto refeicoes_app adicionado a FlutterProjects

---
+++ refeicoes_app/android/app/src/main/kotlin/com/example/refeicoes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.refeicoes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ refeicoes_app/lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:refeicoes_app/models/category.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 0,
+    title: 'Italiano',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 1,
+    title: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  Category(
+    id: 2,
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 3,
+    title: 'Alemã',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 4,
+    title: 'Leve & Saudável',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 5,
+    title: 'Exótica',
+    color: Colors.green,
+  ),
+  Category(
+    id: 6,
+    title: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 7,
+    title: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 8,
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 9,
+    title: 'Verão',
+    color: Colors.teal,
+  )
+];
