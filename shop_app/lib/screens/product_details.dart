319bfd3310c86cff2a8c2e4cf3b49683c92cf164
Weskley_Dev <weskleymdev@gmail.com>
2022-10-16 13:32:53 -0300

    tela de detalhes do produto implementada

--- shop_app/lib/screens/product_details.dart
+++ shop_app/lib/screens/product_details.dart
@@ -8,16 +8,49 @@ class ProductDetails extends StatelessWidget with Capitalize {
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)!.settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(
           capitalize(product.name),
         ),
       ),
-      body: Center(
-        child: Text(
-          capitalize('detalhes do produto'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'R\$${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
         ),
       ),
     );
