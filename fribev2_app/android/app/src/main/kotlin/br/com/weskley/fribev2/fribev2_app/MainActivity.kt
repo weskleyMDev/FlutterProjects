d9a28bcdbc94b751d7ddeb9e313b68ec3a2b88a4
Weskley Moreira <weskleymdev@gmail.com>
2025-07-17 14:23:03 -0300

    projeto fribe v2 adicionado ao repositorio

---
+++ fribev2_app/android/app/src/main/kotlin/br/com/weskley/fribev2/fribev2_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.weskley.fribev2.fribev2_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ fribev2_app/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <charset_converter/charset_converter_plugin.h>
+#include <cloud_firestore/cloud_firestore_plugin_c_api.h>
+#include <file_selector_windows/file_selector_windows.h>
+#include <firebase_auth/firebase_auth_plugin_c_api.h>
+#include <firebase_core/firebase_core_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CharsetConverterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CharsetConverterPlugin"));
+  CloudFirestorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CloudFirestorePluginCApi"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FirebaseAuthPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+}
