6a6acc184dcf6619123969efb7d8f59cea8ad7d3
Weskley Moreira <weskleymdev@gmail.com>
2025-09-03 15:03:29 -0300

    update gradle

--- shop_v2/android/app/build.gradle.kts
+++ shop_v2/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "br.com.weskleym.shopv2.shop_v2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
